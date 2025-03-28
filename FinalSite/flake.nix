2f442f6fa8789fb1dc3af77e7deeb972b281876f
myaumyau <myaumyau@myaumyau.dev>
2025-03-28 10:06:27 -0400

    flake cleanup

--- FinalSite/flake.nix
+++ FinalSite/flake.nix
@@ -1,12 +1,9 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgsveryold.url = "github:nixos/nixpkgs?ref=nixos-21.11";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: let
+  outputs = {nixpkgs, ...}: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
